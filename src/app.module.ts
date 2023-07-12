3d2c8ded9279e72c9dd86c4c218ee83193cde328
JooIcksoo <dlrtn751@gmail.com>
2023-07-12 23:09:55 +0900

    feat: Typeorm 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MemberController } from './business/member/api/member.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './config/typeorm/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeORMConfig)],
   controllers: [MemberController],
   providers: [],
 })
