b5265c9d8a11dfe1853c1f97988ed8c8af71dfdf
JooIcksoo <dlrtn751@gmail.com>
2023-08-08 13:58:48 +0900

    feat: UserService findAll 기능 단위 테스트 작성
    모킹 사용해서 진행

--- test/unit/member.service.spec.ts
+++ test/unit/member.service.spec.ts
@@ -1,23 +1,43 @@
 import { MemberService } from '../../src/business/member/application/member.service';
 import { MemberRepository } from '../../src/business/member/repository/member.repository';
 import { Test, TestingModule } from '@nestjs/testing';
+import { DataSource } from 'typeorm';
+import { MemberModule } from '../../src/business/member/member.module';
+import { find } from 'rxjs';
 
 describe('MemberService 테스트', () => {
   let memberService: MemberService;
-  let memberRepository: MemberRepository;
+  let mockMemberRepository: jest.Mocked<MemberRepository>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MemberService, MemberRepository],
+      providers: [
+        MemberService,
+        {
+          provide: MemberRepository,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue([
+              {
+                id: 1,
+              },
+            ]),
+            find: jest.fn().mockResolvedValue([
+              {
+                id: 1,
+              },
+            ]),
+          },
+        },
+      ],
     }).compile();
 
     memberService = module.get<MemberService>(MemberService);
-    memberRepository = module.get<MemberRepository>(MemberRepository);
   });
 
   describe('사용자 조회 관련 기능 검증', () => {
     it('모든 사용자를 조회한다.', async () => {
       const memberList = await memberService.findAll();
+      console.log(memberList);
       expect(memberList.length).toBeGreaterThan(0);
     });
   });
