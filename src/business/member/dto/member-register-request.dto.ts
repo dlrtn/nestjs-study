ef80f42efa6370b049717afe1f34c28ae2c3f995
JooIcksoo <dlrtn751@gmail.com>
2023-08-10 22:52:46 +0900

    feat: 회원가입용 dto 추가
    swagger docs 설명, 데이터 검증로직 등

---
+++ src/business/member/dto/member-register-request.dto.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class MemberRegisterRequestDto {
+  @IsNotEmpty()
+  @IsEmail()
+  @ApiProperty({ description: '이메일', example: 'test@test.com' })
+  public readonly email: string;
+
+  @IsNotEmpty()
+  @IsStrongPassword()
+  @ApiProperty({ description: '비밀번호', example: '1234' })
+  public readonly password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({ description: '닉네임', example: '테스트' })
+  public readonly nickname: string;
+
+  @IsNotEmpty()
+  @IsPhoneNumber('KR')
+  @ApiProperty({ description: '전화번호', example: '010-1234-5678' })
+  public readonly phoneNumber: string;
+}
