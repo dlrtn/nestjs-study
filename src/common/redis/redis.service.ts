978d8324179b0642e300219b96b6f2c65f73d80a
JooIcksoo <dlrtn751@gmail.com>
2023-08-23 00:05:48 +0900

    feat: redis 모듈 생성

---
+++ src/common/redis/redis.service.ts
@@ -0,0 +1,42 @@
+import { createClient, RedisClientType } from 'redis';
+import { RedisModules } from '@redis/client';
+import { EnvService } from '../env/env.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export default class RedisService {
+  public client: RedisClientType<RedisModules>;
+
+  constructor(private readonly envService: EnvService) {
+    this.client = createClient({
+      url: this.envService.get('REDIS_URL'),
+    });
+
+    this.client.on('error', (error) => {
+      console.log('redis error: ', error);
+    });
+
+    this.client.connect();
+  }
+
+  public async set(key: string, value: string): Promise<void> {
+    await this.client.sendCommand([
+      'SET',
+      key,
+      value,
+      'EX',
+      this.envService.get('REDIS_TTL'),
+    ]);
+  }
+
+  public async get(key: string): Promise<string> {
+    const result = await this.client.sendCommand(['GET', key.toString()]);
+
+    // todo : redis에서 null이나 undefined를 반환할 경우에 대한 처리 (만료된 키로 접근하거나 존재하지 않는 키)
+    if (result === null || result === undefined) {
+      throw new Error('존재하지 않는 키입니다. 다시 시도해 주세요.');
+    }
+
+    return result.toString();
+  }
+}
