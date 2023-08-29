3cb3f432b2af582a43dd4ae91e5a5ed76ae03ea8
dlrtn <dlrtn751@gmail.com>
2023-08-29 14:44:44 +0900

    refactor: redis 모듈 수정
    - app.module에서 선언하도록 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './common/database/database.module';
 import { MemberModule } from './business/member/member.module';
+import { EnvService } from './common/env/env.service';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
 
 @Module({
-  imports: [MemberModule, DatabaseModule],
+  imports: [
+    MemberModule,
+    DatabaseModule,
+    RedisModule.forRoot({
+      config: [
+        {
+          namespace: 'refreshTokenRedis',
+          url: new EnvService().get('REFRESH_TOKEN_REDIS_URL'),
+        },
+      ],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
