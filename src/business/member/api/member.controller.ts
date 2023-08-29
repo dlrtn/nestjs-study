860887b295001f3657d5632d8ea0370a7d54f930
dlrtn <dlrtn751@gmail.com>
2023-08-29 14:48:38 +0900

    refactor: Swagger 및 Response 관련 작업
    - Swagger 관련 변경사항
        - 사용자 생성 API 반환값 표기 수정
        - 사용자 로그인 API 설명 수정
        - 반환값 타입 설명 추가
    - Response 관련 변경사항
        - status 사용해 상태코드 명시
            - 204, 201 등 응답을 보내야할 때, Controller에서 해당 값을 제어할 수 있게
        - 기존 객체 형식 반환에서 값만 반환하게끔 변경
            - 굳이 message를 반환할 필요가 있을까? Swagger에서 다 설명해놓는데, 필요한 값만 보내는게 맞는 것 같다.
    - Controller 일부 리팩토링
        - 변수명 변경으로 코드 가독성을 향상시키고자 했음

--- src/business/member/api/member.controller.ts
+++ src/business/member/api/member.controller.ts
@@ -1,9 +1,15 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { MemberService } from '../application/member.service';
 import { MemberRegisterRequestDto } from '../dto/member-register-request.dto';
 import { MemberLoginRequestDto } from '../dto/member-login-request.dto';
+import { Member } from '../domain/member.entity';
 
 @Controller('/api/members')
 @ApiTags('사용자 API')
@@ -12,39 +18,33 @@ export class MemberController {
 
   @Get()
   @ApiOperation({ summary: '모든 사용자 조회 API' })
-  @ApiOkResponse({ description: '모든 사용자를 조회한다.' })
+  @ApiOkResponse({ description: '모든 사용자를 조회한다.', type: Member })
   async findAll(@Res() res: Response) {
     const memberList = await this.memberService.findAll();
-    return res.json({
-      message: '모든 사용자를 조회한다.',
-      memberList: memberList,
-    });
+    return res.status(HttpStatus.OK).json(memberList);
   }
 
   @Post()
   @ApiOperation({ summary: '사용자 생성 API' })
-  @ApiOkResponse({ description: '사용자를 생성한다.' })
+  @ApiCreatedResponse({ description: '사용자를 생성한다.', type: Member })
   async create(
     @Res() res: Response,
     @Body() request: MemberRegisterRequestDto,
   ) {
-    const member = await this.memberService.register(request);
+    const createdMember = await this.memberService.register(request);
 
-    return res.json({
-      message: '사용자를 생성한다.',
-      member: member,
-    });
+    return res.status(HttpStatus.CREATED).json(createdMember);
   }
 
   @Post('/login')
   @ApiOperation({ summary: '사용자 로그인 API' })
-  @ApiOkResponse({ description: '사용자 인증을 통해 사용자 정보를 반환한다.' })
+  @ApiOkResponse({
+    description: '사용자 인증을 통해 Jwt 토큰을 반환한다.',
+    type: String,
+  })
   async login(@Res() res: Response, @Body() request: MemberLoginRequestDto) {
     const accessToken = await this.memberService.login(request);
 
-    return res.json({
-      message: '사용자 인증을 통해 사용자 정보를 반환한다.',
-      accessToken: accessToken,
-    });
+    return res.status(HttpStatus.OK).json(accessToken);
   }
 }
--- src/business/member/domain/member.entity.ts
+++ src/business/member/domain/member.entity.ts
@@ -7,31 +7,49 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'Member' })
 export class Member {
   @PrimaryColumn()
+  @ApiProperty({
+    description: '사용자 아이디',
+    example: 'uuid',
+  })
   private readonly id: string;
 
   @Column()
+  @ApiProperty({ description: '이메일', example: 'test@test.com' })
   private readonly email: string;
 
   @Column()
+  @ApiProperty({ description: '비밀번호', example: '1234' })
   private readonly password: string;
 
   @Column()
+  @ApiProperty({ description: '닉네임', example: '테스트' })
   private readonly nickname: string;
 
   @Column()
+  @ApiProperty({ description: '전화번호', example: '010-1234-5678' })
   private readonly phoneNumber: string;
 
   @Column()
+  @ApiProperty({ description: '회원 등급', example: MemberGrade.NORMAL })
   private readonly memberGrade: MemberGrade;
 
   @UpdateDateColumn()
+  @ApiProperty({
+    description: '최종 수정일',
+    example: new Date('2021-01-01 00:00:00'),
+  })
   private readonly updatedAt: Date;
 
   @CreateDateColumn()
+  @ApiProperty({
+    description: '생성일',
+    example: new Date('2021-01-01 00:00:00'),
+  })
   private readonly createdAt: Date;
 
   constructor(
