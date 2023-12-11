64c65e0bf6417377fb119ec959d8173b0a048f3b
JooIcksoo <dlrtn751@gmail.com>
2023-12-11 23:33:50 +0900

    refactor: Member Controller 수정
    - 응답용 유틸리티 객체 사용하도록 변경

--- src/business/member/api/member.controller.ts
+++ src/business/member/api/member.controller.ts
@@ -1,25 +1,11 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpStatus,
-  Post,
-  Res,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
-import {
-  ApiBadRequestResponse,
-  ApiCreatedResponse,
-  ApiOkResponse,
-  ApiOperation,
-  ApiTags,
-} from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { MemberService } from '../application/member.service';
 import { MemberRegisterRequestDto } from '../dto/member-register-request.dto';
 import { MemberLoginRequestDto } from '../dto/member-login-request.dto';
-import { Member } from '../domain/member.entity';
-import { JwtAuthGuard } from '../../../common/passport/jwt.auth-guard';
+import { OkResponse } from '../../../common/response/success/ok-response';
+import { CreatedResponse } from '../../../common/response/success/created-response';
 
 @Controller('/api/members')
 @ApiTags('사용자 API')
@@ -28,45 +14,31 @@ export class MemberController {
 
   @Get()
   @ApiOperation({ summary: '모든 사용자 조회 API' })
-  @ApiOkResponse({
-    description: '모든 사용자를 조회한다.',
-    type: Member,
-    isArray: true,
-  })
-  @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({ description: '모든 사용자를 조회한다.' })
   async findAll(@Res() res: Response) {
     const memberList = await this.memberService.findAll();
-    return res.status(HttpStatus.OK).json(memberList);
+
+    return OkResponse.of(res, '사용자 조회에 성공했습니다.', memberList);
   }
 
   @Post()
   @ApiOperation({ summary: '사용자 생성 API' })
-  @ApiCreatedResponse({
-    description: '사용자를 생성한다.',
-    type: Member,
-    status: 201,
-  })
+  @ApiOkResponse({ description: '사용자를 생성한다.' })
   async create(
     @Res() res: Response,
     @Body() request: MemberRegisterRequestDto,
   ) {
-    const createdMember = await this.memberService.register(request);
+    const member = await this.memberService.register(request);
 
-    return res.status(HttpStatus.CREATED).json(createdMember);
+    return CreatedResponse.of(res, '사용자 생성에 성공했습니다.', member);
   }
 
   @Post('/login')
   @ApiOperation({ summary: '사용자 로그인 API' })
-  @ApiOkResponse({
-    description: '사용자 인증을 통해 Jwt 토큰을 반환한다.',
-    type: String,
-  })
-  @ApiBadRequestResponse({
-    description: '입력 정보가 부정확합니다.',
-  })
+  @ApiOkResponse({ description: '사용자 인증을 통해 사용자 정보를 반환한다.' })
   async login(@Res() res: Response, @Body() request: MemberLoginRequestDto) {
     const accessToken = await this.memberService.login(request);
 
-    return res.status(HttpStatus.OK).json(accessToken);
+    return OkResponse.of(res, '사용자 로그인에 성공했습니다.', accessToken);
   }
 }
